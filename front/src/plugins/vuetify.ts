28278e83501e9983b8debf33efcf200b221978ed
yututi <y.tsuchiya0207@gmail.com>
2019-05-17 01:09:39 +0900

    add comment

--- front/src/plugins/vuetify.ts
+++ front/src/plugins/vuetify.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
-import 'vuetify/src/stylus/components/_ripples.styl'
+// vuetifyのバグ？
+// _buttons.stylがないとv-tabsのrippleが動かない。本来なら_buttons.stylが自動で組み込まれるはず？
 import 'vuetify/src/stylus/components/_buttons.styl'
 import ja from 'vuetify/src/locale/ja'
 
