5a50a7694b3873330d2a4f8ccd96a990e5be57be
yututi <y.tsuchiya0207@gmail.com>
2019-05-17 00:50:54 +0900

    improve performance

--- front/src/plugins/vuetify.ts
+++ front/src/plugins/vuetify.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-// import 'vuetify/src/stylus/app.styl'
+import 'vuetify/src/stylus/app.styl'
+import 'vuetify/src/stylus/components/_ripples.styl'
+import 'vuetify/src/stylus/components/_buttons.styl'
 import ja from 'vuetify/src/locale/ja'
 
 Vue.use(Vuetify, {
